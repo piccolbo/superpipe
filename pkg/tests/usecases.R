63a1609dbcc2b0c3cfc6bed2497bdef8bb0c9d31
Antonio Piccolboni <antonio@piccolboni.info>
2015-09-05 22:13:00 -0700

    more examples with latest API

--- pkg/tests/usecases.R
+++ pkg/tests/usecases.R
@@ -1,18 +1,62 @@
+library(dplyr)
+library(superpipe)
+
 mtcars %~>% ~filter(.., carb > 3)
 #filter records with carb greater than 3
-letters %~>% range(sample(1:length(letters))) %~>% ~tail(.., 3)
+letters %~>% Range(sample(1:length(letters))) %~>% ~tail(.., 3)
 #scramble alphabet then take last three
 letters %~>% sample(1:length(letters), 1)
 #random letter
-mtcars %~>% range(c("carb", "cyl"))
+mtcars %~>% Range(c("carb", "cyl"))
 # select two cols
 mtcars %~>% "carb" %~>% mean
-# take carb col and then take mean
+# take carb col and mean
 mtcars %~>% ~carb
+# formula syntax
+mtcars %~>% Range(~carb)
+# or selection of df like select(carb)
 mtcars %~>% ~carb %~>% mean
-# same with formula syntax
+# and then take mean
 mtcars %~>% ~carb %~>% ~mean(x = ..)
 #same with .. syntax, for functions with unusual arg order
-mtcars %~>% row(range(1:10))
-mtcars %~>% row(1)
-#row access
+
+mtcars %~>% 1
+#first col
+mtcars %~>% Row(1)
+#first row as list
+mtcars %~>% Row(Range(1:10))
+# first ten rows
+mtcars %~>% Range(Row(10))
+#row 10 as data frame
+mtcars %~>% Range(Col(10:11))
+# two cols
+mtcars %~>% Col(Range(10))
+# one col as data fram
+mtcars %~>% Col(10)
+#same as vector
+
+ll =
+  list(
+    josh =
+      list(
+        age = 22,
+        gender = "male",
+        employed = TRUE),
+    jeff =
+      list(
+        age = 32,
+        gender = "male",
+        employed = FALSE),
+    jack =
+      list(
+        age = 42,
+        gender = "male",
+        employed = TRUE))
+ll %~>% "josh" %~>% "age"
+# nested list
+
+
+# iteration
+#
+#
+mtcars %@>%
